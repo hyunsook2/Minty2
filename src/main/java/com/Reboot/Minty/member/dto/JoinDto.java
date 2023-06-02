c8e3b6770d3b4adb7f2bedf2d97a091eb1c88514
DESKTOP-UBMCKBR\kosmo <sispa0366@gmail.com>
2023-06-02 15:47:52 +0900

    joindto / 회원가입(join.html) 수정

--- src/main/java/com/Reboot/Minty/member/dto/JoinDto.java
+++ src/main/java/com/Reboot/Minty/member/dto/JoinDto.java
@@ -24,7 +24,7 @@ public class JoinDto {
     private String email;
     @NotBlank(message = "이름은 필수 입력입니다.")
     private String name;
-    @NotEmpty(message = "비밀번호는 필수 입력입니다.")
+
     @Length(min=8,max=16,message="비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
     private String password;
 
